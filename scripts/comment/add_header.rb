50958550d12a15a82564bcbcb67491b9ee13f088
ipotqo <125836089+ipotqo@users.noreply.github.com>
2023-02-24 05:31:55 +0000

    Update add_header.rb

--- scripts/comment/add_header.rb
+++ scripts/comment/add_header.rb
@@ -1,16 +1,17 @@
 #! /usr/bin/env ruby
 
-# ================ CHANGE BELOW ================
+# ================ CHANGE BELOW HERE ================
 
 module SOFTWARE
     TITLE = "MULTIFURCATION"
     AUTHOR = "TIM PLEASETH"
     HANDLE = "@IPOTQO"
     LINK = "HTTPS//WWW.GITHUB.COM/IPOQTO/MULTIFURCATION"
-    LICENSE = "THE UNLICENSE"
+    LICENSE = "UNLICENSE"
+    ROOT = "multifurcation/"
 end
 
-# ================ CHANGE ABOVE ================
+# ================ CHANGE ABOVE HERE ================
 
 module DOXYGEN
 
@@ -20,8 +21,20 @@ module DOXYGEN
 
     FOREWORD =
     "\n"\
-    "#{SOFTWARE::TITLE} COPYRIGHT (C) 2023 #{SOFTWARE::AUTHOR} // #{SOFTWARE::HANDLE}\n\n"\
-    "This file is included in the multifurcation code under The Unlicense.\n"\
+    "#{SOFTWARE::TITLE} COPYRIGHT (C) 2023 // #{SOFTWARE::AUTHOR} #{SOFTWARE::HANDLE}\n\n"\
+    "RELEASED UNDER THE #{SOFTWARE::LICENSE}\n\n"\
+    "This file is included in the #{SOFTWARE::TITLE} code which can found at the link:\n\n"\
+    "\n"\
+    "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY\n"\
+    "EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES\n"\
+    "OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT\n"\
+    "SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,\n"\
+    "INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED\n"\
+    "TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR\n"\
+    "BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN\n"\
+    "CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN\n"\
+    "ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH\n"\
+    "DAMAGE."\
     ""
 
     STATEMENT = FOREWORD.split(/\r?\n|\r/).each { |line| line.prepend(COMMENT) }
@@ -52,7 +65,7 @@ puts "add_header.rb | #{uncommented_file} > #{commented_file}"
 
 File.open(commented_file, 'w') do |fo|
     fo.puts DOXYGEN::START
-    fo.puts DOXYGEN::LABEL( "file", commented_file )
+    fo.puts DOXYGEN::LABEL( "file", SOFTWARE::ROOT + commented_file )
     fo.puts DOXYGEN::LABEL( "brief", "..." )
     fo.puts DOXYGEN::STATEMENT
     fo.puts DOXYGEN::FINISH
